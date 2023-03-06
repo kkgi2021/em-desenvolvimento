e1fd91ed6b8b6bac65f9e8291e508a1c5a2fdee0
claiton <cacagimenis@gmail.com>
2023-03-06 16:27:20 -0300

    modified pages/titles/src/factories/cardsFactory.js

--- pages/titles/src/factories/cardsFactory.js
+++ pages/titles/src/factories/cardsFactory.js
@@ -3,19 +3,16 @@ import CardsView from "./../views/cardsView.js"
 import CardsService from "./../services/cardsService.js"
 
 const cardListWorker = new Worker(`./src/workers/cardListWorker.js`,{type:"module"})
-cardListWorker.onmessage = (msg)=>{
-  console.log('processo principal', msg)
-}
-
-cardListWorker.postMessage('Hey hello!!!')
 
 const [rootPath] = window.location.href.split('/pages/')
 const factory = {
   async initialize() {
-    return CardsController.initialize({
-      worker: cardListWorker,
+    return CardsController.initialize({      
       view: new CardsView(),
-      service: new CardsService({ dbUrl: `${rootPath}/assets/database.json` })
+      service: new CardsService({ 
+        dbUrl: `${rootPath}/assets/database.json`,
+        cardListWorker
+       })
     })
   }
 }
