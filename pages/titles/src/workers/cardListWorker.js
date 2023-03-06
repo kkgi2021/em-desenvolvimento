fae232e178d75c5a4fbdee7f5a9d0c9a87f8dce2
claiton <cacagimenis@gmail.com>
2023-03-06 16:12:57 -0300

    modified:   pages/titles/src/workers/cardListWorker.js

--- pages/titles/src/workers/cardListWorker.js
+++ pages/titles/src/workers/cardListWorker.js
@@ -1,11 +1,11 @@
 onmessage = ({data}) => { 
     let counter = 0                        //funçao global dentro do worker e os dados dele vem dentro
 
-    console.log('activating blocking operation...')
+    console.log('activating blocking operation...', data.maxItems)
     console.time('blocking-op')
     // blocking function
     // 1e5 = 100.000
-    for ( ; counter < 1e5; counter++) console.log('.')
+    for ( ; counter < data.maxItems; counter++) console.log('.')
     console.timeEnd('blocking-op')      //dentro do objeto data 
 
     postMessage(
